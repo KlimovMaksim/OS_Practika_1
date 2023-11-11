f4e4cbb1dc7f4fa470ed9b8636077b83f82aecae
Maksim Klimov <maksimklimov779@gmail.com>
2023-11-11 14:08:23 +0300

    add Person class for кrerialization

---
+++ src/main/java/ru/klimov/exercise/Person.java
@@ -0,0 +1,61 @@
+package ru.klimov.exercise;
+
+public class Person {
+    private String firstName;
+    private String lastName;
+    private int age;
+
+    public Person() {
+    }
+
+    public Person(String firstName, String lastName, int age) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+    }
+
+    public Person(String data) throws Exception {
+        String[] parts = data.split(" ");
+
+        if (parts.length == 3) {
+            this.firstName = parts[0];
+            this.lastName = parts[1];
+            this.age = Integer.parseInt(parts[2]);
+        } else {
+            throw new Exception("Некорректные данные");
+        }
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", age=" + age +
+                '}';
+    }
+}
